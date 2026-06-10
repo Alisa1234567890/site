aea03bc780a2205f17eeffdcc06d8bb1c6b2dd27
Alisa Rudenko <alisa223455@gmail.com>
2026-06-10 14:32:38 +0200

    deploy fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+const params = new URLSearchParams(window.location.search);
+if (params.has('p')) {
+    const path = params.get('p');
+    window.history.replaceState(null, null, '/site' + path + (params.has('q') ? '?' + params.get('q') : '') + window.location.hash);
+}
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/site"> 
-    <App />
-  </BrowserRouter>
+    <StrictMode>
+        <BrowserRouter basename="/site">
+            <App />
+        </BrowserRouter>
+    </StrictMode>
 );
